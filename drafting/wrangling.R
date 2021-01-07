bbf9e47480fe717bfef8e73f98548f7b748aaa9e
Gillian McGinnis <gmcginnis@rstudio.reed.edu>
2021-01-07 14:08:41 -0800

    found workaround of WD issue

---
+++ drafting/wrangling.R
@@ -0,0 +1,69 @@
+
+## Yee haw it's wrangling time
+
+#install.packages("tidyverse")
+library(tidyverse)
+library(lubridate)
+
+
+## ([0-9])\w+\.([0-9])\w+\s\-([0-9])\w+\.([0-9])\w+ #some RegEx that nabs coordinates
+
+#gets list of files which are outside sensors only
+#my_files <- list.files(path = "purpleair_drafting/blob/", pattern = ".+\\(*(outside*)\\w+\\)")
+my_file_paths <- list.files(path = "drafting/blob", pattern = ".+\\(*(outside*)\\w+\\)", full.names = TRUE)
+
+all <- my_file_paths %>%
+  set_names() %>%
+  #map_dfr(.x = my_files, read_csv, .id = "source") #wont work if files not in wd
+  map_dfr(read_csv, .id = "source") %>%
+  mutate(source = str_replace(source, ".\\w+\\/\\w+\\/", "")) #removes filepath name
+
+
+all_mod <- all %>%
+  mutate(created_at = as_datetime(created_at), #converting from character to date time format; tz is in UTC
+         date = date(created_at), #picking out just the date
+         #time = strftime(created_at, format="%H:%M:%S"), # old way of picking time; outputs char
+         time = hms::as_hms(created_at), #picking out just the time
+         time_est = time) %>% #currently working on rounding out the seconds but for now it stands as is
+  mutate(coords = str_extract(source, "([0-9])\\w+\\.([0-9])\\w+\\s\\-([0-9])\\w+\\.([0-9])\\w+"), #grabbing coord data
+         lat = str_extract(coords, "\\d+\\.\\d+\\s"), #latitude (north)
+         lat = str_trim(lat, side = "right"), #removing extra whitespace
+         lat = as.numeric(lat), #converting to numeric from character
+         long = str_extract(coords, "\\-\\d+\\.\\d+"), #longitude (negative here, so west)
+         long = as.numeric(long), #converting to numeric from character
+         loc_tag = str_extract(source, "^[^\\(]+"), #grabbing location tag
+         loc_tag = str_trim(loc_tag, side = "right")) #removing extra whitespace
+
+
+unique_locations <- unique(all_mod$loc_tag) #creates list of unique locations for quick reference
+
+
+date_df <- all_mod %>%
+  drop_na(`PM2.5_CF1_ug/m3`) %>%
+  group_by(loc_tag, date) %>%
+  summarize(mean_pm25 = mean(`PM2.5_CF1_ug/m3`))
+
+ggplot(date_df, aes(x = date, y = mean_pm25, color = loc_tag))+
+  geom_line()
+
+time_df_new <- all_mod %>%
+  drop_na(`PM2.5_CF1_ug/m3`) %>%
+  group_by(loc_tag, time_est) %>%
+  summarize(mean_pm25 = mean(`PM2.5_CF1_ug/m3`))
+
+ggplot(time_df_new, aes(x = time_est, y = mean_pm25, color = loc_tag))+
+  #geom_line()+ #overplotting galore
+  geom_smooth()+
+  scale_x_time()
+
+# time_df <- all_mod %>%
+#   drop_na(`PM2.5_CF1_ug/m3`) %>%
+#   #mutate(time_est = as.POSIXct(round(as.POSIXct(time, format="%H:%M:%S", tz="UTC"), units = "mins"))) %>%
+#   mutate(time_est = hms::as.hms(as.POSIXct(round(created_at), units = "mins"))) %>%
+#   group_by(loc_tag, time_est) %>%
+#   summarize(mean_pm25 = mean(`PM2.5_CF1_ug/m3`))
+#
+# ggplot(time_df, aes(x = time_est, y = mean_pm25, color = loc_tag))+
+#   geom_line(alpha=0.5)+
+#   #geom_smooth()+
+#   scale_x_time()
